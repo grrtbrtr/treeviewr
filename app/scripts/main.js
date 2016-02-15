6382dac1028cce750d0b04fd98b4c9d18eb28509
grrtbrtr <gerrit@mrmoose.be>
2016-02-15 10:25:47 +0100

    Added list and table rendering methods

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,63 @@
 'use strict';
 
 (function($){
-	function json2html(data) {
-		var i, ret = document.createElement('ul'), li;
-    for( i in data) {
-        li = ret.appendChild(document.createElement('li'));
-        li.appendChild(document.createTextNode(i+": "));
-        if( typeof data[i] === "object") li.appendChild(json2html(data[i]));
-        else li.firstChild.nodeValue += data[i];
-    }
-    return ret;
+	function jsonToList(data) {
+		var list = document.createElement('ul');
+		var listItem;
+		var title;
+		var children;
+
+		for (var key in data) {
+			if (typeof(data[key]) === 'object') {
+				if (data[key]['name']) {
+					listItem = list.appendChild(document.createElement('li'));
+					title = listItem.appendChild(document.createElement('span'));
+					title.appendChild(document.createTextNode(data[key]['name']));
+
+					if (data[key]['children']) {
+						listItem.appendChild(jsonToList(data[key]));
+					}
+					
+				}
+				else {
+					list = jsonToList(data[key]);
+				}				
+			}
+		}
+
+		return list;
+	}
+
+	function jsonToTable(data) {
+		var table = document.createElement('table');
+		var row;
+		var titleCell;
+		var contentCell;
+
+		for (var key in data) {
+			if (typeof(data[key]) === 'object') {
+				if (data[key]['name']) {
+					row = table.appendChild(document.createElement('tr'));
+					titleCell = row.appendChild(document.createElement('td'));
+					titleCell.appendChild(document.createTextNode(data[key]['name']));
+
+					if (data[key]['children']) {
+						contentCell = row.appendChild(document.createElement('td'));
+						contentCell.appendChild(jsonToTable(data[key]));
+					}
+					
+				}
+				else {
+					table = jsonToTable(data[key]);
+				}				
+			}
+		}
+
+		return table;
 	}
 
 	function parseData(data) {
-		$('body').append(json2html(data));
+		$('body').append(jsonToTable(data));
 	}
 
 	function jsonLoadDone(data) {
