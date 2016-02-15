af6b3cbf5af7a5ed0dc17a3ad740073e3f9613ef
grrtbrtr <gerrit@mrmoose.be>
2016-02-15 16:23:55 +0100

    Removed boring rendering methods & added SVG rendering method

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,67 +1,135 @@
 'use strict';
 
 (function($){
-	function jsonToList(data) {
-		var list = document.createElement('ul');
-		var listItem;
-		var title;
-		var children;
 
-		for (var key in data) {
-			if (typeof(data[key]) === 'object') {
-				if (data[key]['name']) {
-					listItem = list.appendChild(document.createElement('li'));
-					title = listItem.appendChild(document.createElement('span'));
-					title.appendChild(document.createTextNode(data[key]['name']));
+	var svgNamespace = 'http://www.w3.org/2000/svg';
 
-					if (data[key]['children']) {
-						listItem.appendChild(jsonToList(data[key]));
-					}
-					
-				}
-				else {
-					list = jsonToList(data[key]);
-				}				
-			}
-		}
-
-		return list;
+	function createSVGElement(tag) {
+		return document.createElementNS(svgNamespace, tag);
 	}
 
-	function jsonToTable(data) {
-		var table = document.createElement('table');
-		var row;
-		var titleCell;
-		var contentCell;
+	function createSvgTextElement(text, x, y) {
+		var MAXIMUM_CHARS_PER_LINE = 16,
+    		LINE_HEIGHT = 16;
 
-		for (var key in data) {
-			if (typeof(data[key]) === 'object') {
-				if (data[key]['name']) {
-					row = table.appendChild(document.createElement('tr'));
-					titleCell = row.appendChild(document.createElement('td'));
-					titleCell.appendChild(document.createTextNode(data[key]['name']));
+		var textEl = createSVGElement('text');
+		textEl.setAttributeNS(null, 'style', 'text-anchor: middle;');
+		textEl.setAttributeNS(null, 'font-size', 12);
+		textEl.setAttributeNS(null, 'font-family', 'Open Sans');
+		textEl.setAttributeNS(null, 'font-weight', 300);
+		textEl.setAttributeNS(null, 'fill', '#434649');
+		textEl.setAttributeNS(null, 'x', x);
+		textEl.setAttributeNS(null, 'y', y);
 
-					if (data[key]['children']) {
-						contentCell = row.appendChild(document.createElement('td'));
-						contentCell.appendChild(jsonToTable(data[key]));
-					}
-					
+		var words = text.split(' ');
+    var line = '';
+
+    for (var i = 0; i < words.length; i++) {
+      var testLine = line + words[i] + ' ';
+      if (testLine.length + 1 > MAXIMUM_CHARS_PER_LINE) {
+        var svgTSpan = createSVGElement('tspan');
+        svgTSpan.setAttributeNS(null, 'x', x);
+        svgTSpan.setAttributeNS(null, 'y', y);
+        svgTSpan.appendChild(document.createTextNode(line));
+        textEl.appendChild(svgTSpan);
+
+        line = words[i];
+        y += LINE_HEIGHT;
+      } else {
+        line = testLine;
+      }
+  	}
+
+  	var svgTSpan = createSVGElement('tspan');
+    svgTSpan.setAttributeNS(null, 'x', x);
+    svgTSpan.setAttributeNS(null, 'y', y);
+    svgTSpan.appendChild(document.createTextNode(line));
+    textEl.appendChild(svgTSpan);
+
+    return textEl;
+	}
+
+	function renderTreeElement(data, container) {
+		var shape, text, childContainer;
+		var x = 0;
+		var y = 0;
+
+		if (data.name) {
+			shape = createSVGElement('circle');
+			shape.setAttributeNS(null, 'cx', 50);
+			shape.setAttributeNS(null, 'cy', 50);
+			shape.setAttributeNS(null, 'r',  50);
+			shape.setAttributeNS(null, 'fill', '#e9ebec');
+			container.appendChild(shape);
+
+			text = createSvgTextElement(data.name, 50, 50);
+			container.appendChild(text);
+				
+			if (data.children && data.children.length > 0) {
+				childContainer = createSVGElement('svg');
+				container.appendChild(childContainer);
+				for (var i = 0; i < data.children.length; i++) {
+					var child = createSVGElement('svg');
+					childContainer.appendChild(child);
+					renderTreeElement(data.children[i], child);
+					child.setAttributeNS(null, 'x', x);
+					child.setAttributeNS(null, 'y', y);
+					x = childContainer.getBBox().width + 25;
 				}
-				else {
-					table = jsonToTable(data[key]);
-				}				
+				x += childContainer.getBBox().width;
+				childContainer.setAttributeNS(null, 'x', 0);
+				childContainer.setAttributeNS(null, 'y', y + 150);
+				console.info(data.name + ': ' + childContainer.getBBox().width);
 			}
 		}
 
-		return table;
+		container.setAttributeNS(null, 'x', x);
+		container.setAttributeNS(null, 'y', y);
+
+		//return container;
+	}
+
+	function renderSvg(data) {
+		var container = document.getElementById('treeviewr');
+		var svg = createSVGElement('svg');
+		var el;
+		var margin = 50;
+		container.appendChild(svg);
+
+		var x = margin;
+		var y = margin;
+		var maxH = 0;
+		var elBBox;
+
+		if (data.nodes) {
+			for (var key in data.nodes) {
+				el = createSVGElement('svg');
+				svg.appendChild(el);
+				renderTreeElement(data.nodes[key], el);
+				el.setAttributeNS(null, 'x', x);
+				el.setAttributeNS(null, 'y', y);
+
+				elBBox = el.getBBox();
+				x += elBBox.width + 100;
+				if (elBBox.height > maxH) {
+					maxH = elBBox.height;
+				}
+			}
+		}	else {
+			console.error('Incorrect JSON input: no "nodes" element defined on root level.');
+		}
+
+		svg.setAttribute('width', x);
+		svg.setAttribute('height', maxH + (2 * margin));
+		svg.setAttribute('style', 'vertical-align: top;');
+		
 	}
 
 	function parseData(data) {
-		$('body').append(jsonToTable(data));
+		$('body').append(renderSvg(data));
 	}
 
 	function jsonLoadDone(data) {
-		console.log(data);
 		parseData(data);
 	}
 
