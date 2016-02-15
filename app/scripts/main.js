1712b6aaa70331b319aa4a821060369759b981e1
grrtbrtr <gerrit@mrmoose.be>
2016-02-15 17:32:17 +0100

    Removed jQuery dependency and added connector lines from parents to children

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
-(function($){
+(function(){
 
 	var svgNamespace = 'http://www.w3.org/2000/svg';
+	var svgContainer;
+	var elementRadius = 50;
 
 	function createSVGElement(tag) {
 		return document.createElementNS(svgNamespace, tag);
@@ -49,52 +51,102 @@
     return textEl;
 	}
 
-	function renderTreeElement(data, container) {
-		var shape, text, childContainer;
+	function signum(x) {
+    return (x < 0) ? -1 : 1;
+	}
+	
+	function absolute(x) {
+    return (x < 0) ? -x : x;
+	}
+
+	function connect(childEl, parentEl, container) {
+		var path = createSVGElement('path');
+		path.setAttributeNS(null, 'd', 'M0 0');
+		path.setAttributeNS(null, 'stroke', '#97999B');
+		path.setAttributeNS(null, 'fill', 'none');
+		path.setAttributeNS(null, 'stroke-width', '2px');
+		container.appendChild(path);
+
+		var startX, startY, endX, endY;
+
+		// Calculate positions
+		var childElX = parseInt(childEl.getAttributeNS(null, 'x'), 10);
+		var parentElX = parseInt(parentEl.getAttributeNS(null, 'x'), 10);
+		childElX = isNaN(childElX) ? 0 : childElX;
+		parentElX = isNaN(parentElX) ? 0 : parentElX;
+
+		var startX = parentElX + elementRadius;
+		var startY = elementRadius * 2;
+		var endX = childElX + elementRadius;
+		var endY = 3 * elementRadius;
+
+		var deltaX = (endX - startX) * 0.3333;
+    var deltaY = (endY - startY) * 0.3333;
+
+    // for further calculations which ever is the shortest distance
+    var delta = deltaY < absolute(deltaX) ? deltaY : absolute(deltaX);
+
+    var arc1 = 0; var arc2 = 1;
+    if (startX > endX) {
+      arc1 = 1;
+      arc2 = 0;
+    }
+
+    path.setAttributeNS(null, 'd',  'm'  + startX + ' ' + startY +
+                    ' v' + (((endY - startY) / 2) - delta) +
+                    ' a' + delta + ' ' +  delta + ' 0 0 ' + arc1 + ' ' + delta + ' ' + delta +
+                    ' H' + (endX - delta * signum(deltaX)) + 
+                    ' a' + delta + ' ' +  delta + ' 0 0 ' + arc2 + ' ' + delta + ' ' + delta +
+                    ' v' + (((endY - startY) / 2) - delta));
+	}
+
+	function renderTreeElement(data, container, parent) {
+		var bg, text, childContainer;
 		var x = 0;
 		var y = 0;
 
 		if (data.name) {
-			shape = createSVGElement('circle');
-			shape.setAttributeNS(null, 'cx', 50);
-			shape.setAttributeNS(null, 'cy', 50);
-			shape.setAttributeNS(null, 'r',  50);
-			shape.setAttributeNS(null, 'fill', '#e9ebec');
-			container.appendChild(shape);
+			bg = createSVGElement('circle');
+			bg.setAttributeNS(null, 'cx', elementRadius);
+			bg.setAttributeNS(null, 'cy', elementRadius);
+			bg.setAttributeNS(null, 'r',  elementRadius);
+			bg.setAttributeNS(null, 'fill', '#e9ebec');
+			container.appendChild(bg);
 
-			text = createSvgTextElement(data.name, 50, 50);
+			text = createSvgTextElement(data.name, elementRadius, elementRadius);
 			container.appendChild(text);
 				
 			if (data.children && data.children.length > 0) {
 				childContainer = createSVGElement('svg');
 				container.appendChild(childContainer);
+				var child;
 				for (var i = 0; i < data.children.length; i++) {
-					var child = createSVGElement('svg');
+					child = createSVGElement('svg');
 					childContainer.appendChild(child);
+
 					renderTreeElement(data.children[i], child);
 					child.setAttributeNS(null, 'x', x);
 					child.setAttributeNS(null, 'y', y);
-					x = childContainer.getBBox().width + 25;
+
+					// Add connector to parent
+					connect(child, bg, container);
+
+					x = childContainer.getBBox().width + (elementRadius / 2);
 				}
-				x += childContainer.getBBox().width;
 				childContainer.setAttributeNS(null, 'x', 0);
-				childContainer.setAttributeNS(null, 'y', y + 150);
-				console.info(data.name + ': ' + childContainer.getBBox().width);
+				childContainer.setAttributeNS(null, 'y', y + (3 * elementRadius));
 			}
 		}
 
 		container.setAttributeNS(null, 'x', x);
 		container.setAttributeNS(null, 'y', y);
-
-		//return container;
 	}
 
 	function renderSvg(data) {
-		var container = document.getElementById('treeviewr');
 		var svg = createSVGElement('svg');
 		var el;
 		var margin = 50;
-		container.appendChild(svg);
+		svgContainer.appendChild(svg);
 
 		var x = margin;
 		var y = margin;
@@ -122,37 +174,51 @@
 		svg.setAttribute('width', x);
 		svg.setAttribute('height', maxH + (2 * margin));
 		svg.setAttribute('style', 'vertical-align: top;');
-		
-	}
-
-	function parseData(data) {
-		$('body').append(renderSvg(data));
 	}
 
 	function jsonLoadDone(data) {
-		parseData(data);
+		renderSvg(data);
 	}
 
-	function jsonLoadFail(error) {
-		console.error(error);
-	}
-
-	function jsonLoadAlways() {
-		console.info('getJSON call executed');
+	function jsonLoadFail(xhr) {
+		console.error('Error loading JSON file: ' + xhr.status + ': ' + xhr.statusText);
 	}
 
 	function loadTree() {
-		$.getJSON('tree.json')
-			.done(function(data) { jsonLoadDone(data); })
-			.fail(function(jqXHR, textStatus, error) { jsonLoadFail(error); })
-			.always(function() { jsonLoadAlways(); });
+		var path;
+		var xhr = null;
+
+		svgContainer = document.getElementById('treeviewr');
+		path = svgContainer.getAttribute('data-file');
+
+    if (window.ActiveXObject) {
+    	xhr = new ActiveXObject('Microsoft.XMLHTTP');
+    } else if (window.XMLHttpRequest) {
+    	xhr = new XMLHttpRequest();
+    }
+
+    if (xhr) {
+      xhr.onreadystatechange = function() {
+      	if (xhr.readyState === XMLHttpRequest.DONE) {
+	        if (xhr.status === 200) {
+	          jsonLoadDone(JSON.parse(xhr.responseText));
+	        } else {
+	          jsonLoadFail(xhr);
+	        }
+	      }
+	    };
+	    xhr.open('GET', path, true);
+	    xhr.send();
+    } else {
+    	console.error('What?! Your naughty little browser can not handle AJAX requests. Get your shit together and use a real browser!');
+    }    
 	}
 
 	function init() {
 		loadTree();
 	}
 
-  $(function(){
-    init();
-  });
-})(jQuery);
+	document.addEventListener("DOMContentLoaded", function(event) {
+		init();
+	});
+})();
